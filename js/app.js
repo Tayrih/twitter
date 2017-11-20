69dfbb06b6137a114e22067bf637dd1255acb21a
tayrih <tahysluana@gmail.com>
2017-11-20 09:01:06 -0500

    corrije indentacion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var textContainer = document.getElementById('tweets');
 var boxTweet = document.getElementById('box-tweet');
-var camTweet = document.getElementById("camera");
+var camTweet = document.getElementById('camera');
 var postTweet = document.getElementsByClassName('text-tweet');
 var buton = document.getElementsByClassName('white right');
 var sizeH = 0;
@@ -21,19 +21,19 @@ function createText() {
   var textTweet = document.createElement('textarea');
   var buttonTweet = document.createElement('button');
 
-  textTweet.setAttribute('class','text-tweet');
-  textTweet.setAttribute('placeholder','¿Qué está pasando?');
-  textTweet.setAttribute('rows','2');
-  textTweet.setAttribute('value','text');
-  textTweet.setAttribute('scrollbar','false');
+  textTweet.setAttribute('class', 'text-tweet');
+  textTweet.setAttribute('placeholder', '¿Qué está pasando?');
+  textTweet.setAttribute('rows', '2');
+  textTweet.setAttribute('value', 'text');
+  textTweet.setAttribute('scrollbar', 'false');
 
-  buttonTweet.textContent = "Tweetear";
-  buttonTweet.setAttribute('class','white right');
+  buttonTweet.textContent = 'Tweetear';
+  buttonTweet.setAttribute('class', 'white right');
 
   boxTweet.appendChild(textTweet);
   boxTweet.setAttribute('class', 'txta-input');
   boxTweet.appendChild(buttonTweet);
-  //DESHABILITA EL BOTON AL CREARSE
+  // DESHABILITA EL BOTON AL CREARSE
   buton[0].disabled = true;
   buton[0].style.backgroundColor = '#b8e1fa';
   postTweet[0].focus();
@@ -44,17 +44,16 @@ function createText() {
 
 
   sizeH = postTweet[0].scrollHeight;
-// evento para verificar que no se ingrese texto vacio, anida una funcion vacia
-  postTweet[0].addEventListener('keyup', function (event) {
-     if (postTweet[0].value.substr(0,1) === " ") { // LEEMOS SI LO QUE PRESIONO AL INICIO ES UN ESPACIO
+  // evento para verificar que no se ingrese texto vacio, anida una funcion vacia
+  postTweet[0].addEventListener('keyup', function(event) {
+    if (postTweet[0].value.substr(0, 1) === ' ') { // LEEMOS SI LO QUE PRESIONO AL INICIO ES UN ESPACIO
       postTweet[0].value = postTweet[0].value.substr(1); // BORRAMOS O REGRESAMOS AL ESTADO INICIAL
       buton[0].disabled = true;
       buton[0].style.backgroundColor = '#b8e1fa';
-
     } else {
       buton[0].disabled = false;
-	    buton[0].style.backgroundColor = '#50b6f5';
-      countTweets(event); //funcion qque no funciona
+      buton[0].style.backgroundColor = '#50b6f5';
+      countTweets(event); // funcion qque no funciona
       resizeTextArea(event);
       buton[0].addEventListener('click', createTweets);
     }
@@ -62,79 +61,74 @@ function createText() {
 }
 
 // evento para crear  tweets y agregarlos al html
-function createTweets (event){
-  console.log('holi');
-    if(postTweet[0].value) {
-        var p = document.createElement('p');
-        var spanTime = document.createElement('span');
-        p.setAttribute('class','text-tweet');
-      // p.textContent = postTweet[0].value;
-        p.textContent = time() + '    ' + postTweet[0].value;
-        textContainer.appendChild(p);
-        postTweet[0].value = '';
-    }
-
+function createTweets(event) {
+  if (postTweet[0].value) {
+    var paragraph = document.createElement('p');
+    var spanTime = document.createElement('span');
+    paragraph.setAttribute('class', 'text-tweet');
+    paragraph.textContent = time() + '    ' + postTweet[0].value;
+    textContainer.appendChild(paragraph);
+    postTweet[0].value = '';
+  }
 }
 
 function countTweets(event) {
-
-	var count = postTweet[0].value.length;
-	var show = 140 - count*1;
+  var count = postTweet[0].value.length;
+  var show = 140 - count * 1;
   var contador = document.getElementById('contador');
-	contador.textContent = show;
+  contador.textContent = show;
 
-	if (count > 0 && count < 119) {
-		contador.style.color = '#50b6f5';
-	} else if (count >= 120 && count <= 130) {
-		contador.style.color = "#f7b101";
-	} else if (count > 130 && count <= 140) {
-		contador.style.color = "#d70908";
-	} else {
+  if (count > 0 && count < 119) {
+    contador.style.color = '#50b6f5';
+  } else if (count >= 120 && count <= 130) {
+    contador.style.color = '#f7b101';
+  } else if (count > 130 && count <= 140) {
+    contador.style.color = '#d70908';
+  } else {
     buton[0].disabled = true;
     buton[0].style.backgroundColor = '#b8e1fa';
-	}
+  }
 }
 
 function resizeTextArea(event) {
-	var colsIn = postTweet[0].getAttribute('cols');
-	var rowsIn = postTweet[0].getAttribute('rows');
+  var colsIn = postTweet[0].getAttribute('cols');
+  var rowsIn = postTweet[0].getAttribute('rows');
   var rowsFinal;
-	if (postTweet[0].value.length !== 0) {
-		if (event.keyCode == 13) {
-			rowsFinal = parseInt(rowsIn) + 1;
-			postTweet[0].setAttribute('rows', rowsFinal);
-		} else if(postTweet[0].scrollHeight > sizeH){
+  if (postTweet[0].value.length !== 0) {
+    if (event.keyCode === 13) {
+      rowsFinal = parseInt(rowsIn) + 1;
+      postTweet[0].setAttribute('rows', rowsFinal);
+    } else if (postTweet[0].scrollHeight > sizeH) {
       rowsFinal = parseInt(rowsIn) + 1;
       postTweet[0].setAttribute('rows', rowsFinal);
       sizeH = postTweet[0].scrollHeight;
-    } else if(postTweet[0].scrollHeight < sizeH){
+    } else if (postTweet[0].scrollHeight < sizeH) {
       rowsFinal = parseInt(rowsIn) - 1;
       postTweet[0].setAttribute('rows', rowsFinal);
       sizeH = postTweet[0].scrollHeight;
     }
-	} else {
-		postTweet[0].setAttribute('rows', 2);
-	}
+  } else {
+    postTweet[0].setAttribute('rows', 2);
+  }
 }
 
 
-
 function time() {
-	var date = new Date();
-	var hours = date.getHours();
-	var min = date.getMinutes();
-	var time;
-	if (min < 10) {
-		min = '0' + min;
-	}
+  var date = new Date();
+  var hours = date.getHours();
+  var min = date.getMinutes();
+  var time;
+  if (min < 10) {
+    min = '0' + min;
+  }
 
   if (hours > 12 && hours <= 24) {
-		time = hours-12 + ':' + min + ' pm: ';
-	} else if (hours == 12){
-		time = hours + ':' + min + ' m: ';
-	} else {
+    time = hours - 12 + ':' + min + ' pm: ';
+  } else if (hours === 12) {
+    time = hours + ':' + min + ' m: ';
+  } else {
     time = hours + ':' + min + ' am: ';
   }
 
-	return time;
+  return time;
 }
